da76da831ca22a6cc65553db69ec1a42933cd8da
Gabriel <gabriel.mazzo@aluno.senai.br>
2026-08-13 11:16:35 -0300

    Alterações 13/8

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,12 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Aluno
- */
+
+import java.util.Scanner;
+
 public class NewClass {
-    
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        int opcao;
+        
+        while(true){
+   System.out.println("LISTA DE TAREFAS");
+        System.out.println("1 - Adicionar tarefa");
+        System.out.println("2 - Listar tarefas");
+        System.out.println("3 - Concluir tarefa");
+        System.out.println("4 - Excluir tarefa");
+        System.out.println("5 - Sair.");
+        
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+        
+        if(opcao == 1){
+            System.out.println("Digite a tarefa: ");
+            
+            System.out.println("Tarefa adicionada com sucesso!");
+            
+        }
+        
+        }   
+    }
 }
