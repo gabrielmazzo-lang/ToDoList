2cd57570b9c5b9a96f7f737eb8a83a2bdb664cc4
gabriel <gabriel.mazzo@aluno.senai.br>
2026-08-19 10:57:02 -0300

    Alterações aula 19/08

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,30 +1,51 @@
-
-
 import java.util.Scanner;
 
 public class NewClass {
+
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-        int opcao;
+        Scanner scanner = new Scanner(System.in);
         
-        while(true){
-   System.out.println("LISTA DE TAREFAS");
-        System.out.println("1 - Adicionar tarefa");
-        System.out.println("2 - Listar tarefas");
-        System.out.println("3 - Concluir tarefa");
-        System.out.println("4 - Excluir tarefa");
-        System.out.println("5 - Sair.");
+        String[] tarefas = new String[10];
+        boolean[] concluidas = new boolean[10];
         
-        opcao = scanner.nextInt();
-        scanner.nextLine();
+        int opcao = 0;
         
-        if(opcao == 1){
-            System.out.println("Digite a tarefa: ");
+        while(opcao !=5){
+            System.out.println("Lista De Tarefas");
+            System.out.println("1.Adicionar tarefa");
+            System.out.println("2.Listar tarefa");
+            System.out.println("3.Concluir tarefa");
+            System.out.println("4.Excluir tarefa");
+            System.out.println("5.Sair");
             
-            System.out.println("Tarefa adicionada com sucesso!");
+            System.out.println("Escolha uma opcao: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
             
+            switch(opcao){
+                case 1:
+                    int posicaoLivre = -1;
+                    
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i] == null){
+                            posicaoLivre = i;
+                            break;
+                            
+                        }
+                    }
+                    if (posicaoLivre == -1){
+                        System.out.println("A lista de tarefas está cheia!");
+               }
+                 else{
+               System.out.println("Digite a tarefa: ");
+               tarefas[posicaoLivre] = scanner.nextLine();
+               System.out.println("Tarefa adicionada com sucesso!");
+               concluidas[posicaoLivre] = false;
+                        System.out.println("Tarefa adicionada com sucesso");
+           }
+                    
+           break;         
+          }
         }
-        
-        }   
+      }
     }
-}
