d0ffd652f9cfb0e364265d1b3917b39284e9fe3a
gabriel <gabriel.mazzo@aluno.senai.br>
2026-08-20 09:04:22 -0300

    Alterações dia 20/08

--- src/NewClass.java
+++ src/NewClass.java
@@ -84,7 +84,32 @@ public class NewClass {
                     else{
                         System.out.println("Tarefa invalida.");
                     }
-          }
+                case 4:
+                    System.out.println("Digite o numero da tarefa que deseja excluir ");
+                    int numeroExcluir = scanner.nextInt();
+                    
+                    int indiceExcluir = numeroExcluir - 1;
+                    
+                    if(indiceExcluir >= 0 &&
+                            indiceExcluir < tarefas.length &&
+                            tarefas[indiceExcluir] != null){
+                        tarefas[indiceExcluir] = null;
+                        concluidas[indiceExcluir] = false;
+                        
+                        System.out.println("Tarefa excluida com sucesso!");
+                    }else{
+                        System.out.println("Tarefa invalida!");
+                    }
+                       break;
+                case 5:
+                    System.out.println("Programa encerrado. Até");
+                    break;
+                    
+                default:
+                    System.out.println("Opção invalida!");
+                    break;
+                   
+            }
         }
       }
     }
